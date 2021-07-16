5d659c551143cf5dec177963164f3e2f32be5a9c
Joshua Clements <87177073+KyomiDev@users.noreply.github.com>
2021-07-16 11:52:03 +0100

    SignalEmbed

---
+++ src/structures/SignalEmbed.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require('discord.js');
+
+class SignalEmbed extends MessageEmbed {
+	constructor(messageInteraction, data = {}) {
+		super(data);
+
+		this.setTimestamp()
+			.setColor(messageInteraction.guild.me.displayHexColor)
+			.setFooter(messageInteraction.member.displayName, messageInteraction?.author.displayAvatarURL({ dynamic: true }) || messageInteraction?.user.displayAvatarURL({ dynamic: true }));
+	}
+}
+
+module.exports = SignalEmbed;
