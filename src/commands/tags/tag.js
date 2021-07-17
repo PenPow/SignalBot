4aa0c694e6468e7d24d0631292c237c9d7a2d52c
Joshua Clements <87177073+KyomiDev@users.noreply.github.com>
2021-07-17 19:16:54 +0100

    slash commands

--- src/commands/tags/tag.js
+++ src/commands/tags/tag.js
@@ -323,7 +323,159 @@ module.exports = class TagCommand extends Command {
 	}
 
 	async slashRun(interaction, args) {
+		await this.client.db.ensure(`guild_tags_${interaction.guild.id}`, []);
+		await this.client.db.ensure('guild_tags', []);
+		switch(args.first().name) {
+		case 'guide': {
+			const embed = new SignalEmbed(interaction)
+				.setTitle(':green_book: Guide')
+				.setDescription('To see how to use the tag system, view the guide [here](https://github.com/PenPow/SignalBot/wiki/Tag-System)');
 
+			interaction.reply({ embeds: [embed], ephemeral: true });
+			break;
+		}
+		case 'list': {
+			const tags = this.client.db.get(`guild_tags_${interaction.guild.id}`) || [];
+			const embed = new SignalEmbed(interaction).setTitle(`${store} Tags in ${interaction.guild.name}`);
+			let description;
+			for(let i = 0; i < tags.length; i++) {
+				description = tags.map((tag) => {
+					return '`' + tag.name + '`';
+				}).join(', ');
+			}
+
+			embed.setDescription(description || 'No Tags Found');
+			interaction.reply({ embeds: [embed], ephemeral: true });
+			break;
+		}
+		case 'edit': {
+			const embed = new SignalEmbed(interaction);
+			const tags = this.client.db.get(`guild_tags_${interaction.guild.id}`) || [];
+
+			let found = false;
+			for(let i = 0; i < tags.length; i++) {
+				if(tags[i].name === args.get('edit').options.get('name')?.value.replace(/ /g, '-').replace(/(\r\n|\n|\r)/gm, '').toLowerCase()) {
+					found = true;
+					break;
+				}
+			}
+
+			if(!found) {
+				embed.setTitle(`${store} No Tag Found`)
+					.setDescription('No tag with that name was found');
+
+				return interaction.reply({ embeds: [embed], ephemeral: true });
+			}
+
+			for(let i = 0; i < tags.length; i++) {
+				if(tags[i].name === args.get('edit').options.get('name')?.value.replace(/ /g, '-').replace(/(\r\n|\n|\r)/gm, '').toLowerCase()) {
+					tags[i].content = args.get('edit').options.get('content')?.value;
+					tags[i].modifiedAt = `<t:${ (new Date().getTime() / 1000).toFixed(0)}:F>`;
+					tags[i].modified.user = { tag: interaction.user.tag, id: interaction.user.id };
+				}
+			}
+
+			this.client.db.set(`guild_tags_${interaction.guild.id}`, tags);
+			embed.setTitle(`${store} Edited Tag`)
+				.setDescription(`Tag Successfully Edited, access it through \`${this.client.db.get(`${interaction.guild.id}_prefix`)}${args.get('edit').options.get('name')?.value.replace(/(\r\n|\n|\r)/gm, '').replace(/ /g, '-').toLowerCase()}\``);
+
+			await interaction.reply({ embeds: [embed], ephemeral: true });
+
+			break;
+		}
+		case 'delete': {
+			await this.client.db.ensure(`guild_tags_${interaction.guild.id}`, []);
+			const tags = this.client.db.get(`guild_tags_${interaction.guild.id}`);
+			const embed = new SignalEmbed(interaction);
+
+			if(!tags) {
+				embed
+					.setTitle(`${store} No Tags Found`)
+					.setDescription('There are no tags in this server');
+				return interaction.reply({ embeds: [embed], ephemeral: true });
+			}
+
+			for(let i = 0; i < tags.length; i++) {
+				if(tags[i].name.toLowerCase() === args.get('delete').options.get('name')?.value.replace(/(\r\n|\n|\r)/gm, '').replace(/ /g, '-').toLowerCase()) {
+					tags.splice(i, 1);
+					this.client.db.set(`guild_tags_${interaction.guild.id}`, tags);
+
+					embed.setTitle(`${store} Tag Removed`)
+						.setDescription('Successfully removed the tag!');
+
+					return interaction.reply({ embeds: [embed], ephemeral: true });
+				}
+			}
+
+			embed
+				.setTitle(`${store} No Tag Found`)
+				.setDescription(`There are no tags in this server named \`${args.get('delete').options.get('name')?.value.replace(/(\r\n|\n|\r)/gm, '').replace(/ /g, '-').toLowerCase()}\``);
+			interaction.reply({ embeds: [embed], ephemeral: true });
+			break;
+		}
+		case 'show': {
+			await this.client.db.ensure(`guild_tags_${interaction.guild.id}`, []);
+			const tags = this.client.db.get(`guild_tags_${interaction.guild.id}`);
+
+			if(!tags) {
+				const embed = new SignalEmbed(interaction)
+					.setTitle(`${store} No Tags Found`)
+					.setDescription('There are no tags in this server');
+				return interaction.reply({ embeds: [embed], ephemeral: true });
+			}
+
+			for(let i = 0; i < tags.length; i++) {
+				if(tags[i].name.toLowerCase() === args.get('show').options.get('name')?.value.replace(/(\r\n|\n|\r)/gm, '').replace(/ /g, '-').toLowerCase()) {
+					const embed = new SignalEmbed(interaction)
+						.setDescription(`❯ **Name**
+								\`${tags[i].name}\`
+								❯ **User**
+								\`${tags[i].user.tag}\` (${tags[i].user.id})
+								❯ **Uses**
+								\`${tags[i].uses}\`
+								❯ **Created**
+								${tags[i].createdAt}
+								❯ **Modified**
+								${tags[i].modifiedAt}
+								❯ **Last Modified By**
+								\`${tags[i].modified.user.tag}\` (${tags[i].modified.user.id})
+								`);
+
+					return interaction.reply({ embeds: [embed], ephemeral: true });
+				}
+			}
+
+			const embed = new SignalEmbed(interaction)
+				.setTitle(`${store} No Tag Found`)
+				.setDescription(`There are no tags in this server named \`${args.get('show').options.get('name')?.value.replace(/(\r\n|\n|\r)/gm, '').replace(/ /g, '-').toLowerCase()}\``);
+			interaction.reply({ embeds: [embed], ephemeral: true });
+			break;
+		}
+		case 'create': {
+			await this.client.db.ensure(`guild_tags_${interaction.guild.id}`, []);
+
+			if(!this.client.db.includes('guild_tags', interaction.guild.id)) this.client.db.push('guild_tags', interaction.guild.id);
+			const tags = this.client.db.get(`guild_tags_${interaction.guild.id}`) || [];
+
+			for(let i = 0; i < tags.length; i++) {
+				if(tags[i].name.toLowerCase() === args.get('create').options.get('name')?.value.replace(/(\r\n|\n|\r)/gm, '').replace(/ /g, '-').toLowerCase()) {
+					embed.setTitle(`${store} Tag Already Exists`)
+						.setDescription('A tag with this name already exists, consider modifying it');
+
+					await interaction.reply({ embeds: [embed], ephemeral: true });
+
+					return;
+				}
+			}
+			this.client.db.push(`guild_tags_${interaction.guild.id}`, { uses: 0, name: args.get('create').options.get('name')?.value.replace(/(\r\n|\n|\r)/gm, '').replace(/ /g, '-').toLowerCase(), content: args.get('create').options.get('content')?.value, user: { tag: interaction.user.tag, id: interaction.user.id }, createdAt: `<t:${ (new Date().getTime() / 1000).toFixed(0)}:F>`, modifiedAt: `<t:${ (new Date().getTime() / 1000).toFixed(0)}:F>`, modified: { user: { tag: interaction.user.tag, id: interaction.user.id } } });
+			const embed = new SignalEmbed(interaction).setTitle(`${store} Creating a New Tag (3/3)`)
+				.setDescription(`Tag Successfully Created, access it through \`${this.client.db.get(`${interaction.guild.id}_prefix`)}${args.get('create').options.get('name')?.value.replace(/(\r\n|\n|\r)/gm, '').replace(/ /g, '-').toLowerCase()}\``);
+
+			await interaction.reply({ embeds: [embed], ephemeral: true });
+
+			break;
+		}
+		}
 	}
 
 	generateSlashCommand() {
@@ -334,21 +486,62 @@ module.exports = class TagCommand extends Command {
 				name: 'create',
 				type: 'SUB_COMMAND',
 				description: 'Creates a new tag',
+				options: [{
+					name: 'name',
+					type: 'STRING',
+					description: 'The name of the tag',
+					required: true,
+				},
+				{
+					name: 'content',
+					type: 'STRING',
+					description: 'The content of the tag',
+					required: true,
+				}],
+			},
+			{
+				name: 'list',
+				type: 'SUB_COMMAND',
+				description: 'Lists the tags for the server',
 			},
 			{
 				name: 'show',
 				type: 'SUB_COMMAND',
 				description: 'Shows an existing tag',
+				options: [{
+					name: 'name',
+					type: 'STRING',
+					description: 'The name of the tag',
+					required: true,
+				}],
 			},
 			{
 				name: 'edit',
 				type: 'SUB_COMMAND',
 				description: 'Edits a tag',
+				options: [{
+					name: 'name',
+					type: 'STRING',
+					description: 'The name of the tag',
+					required: true,
+				},
+				{
+					name: 'content',
+					type: 'STRING',
+					description: 'The content of the tag',
+					required: true,
+				}],
 			},
 			{
 				name: 'delete',
 				type: 'SUB_COMMAND',
 				description: 'Deletes a tag',
+				options: [{
+					name: 'name',
+					type: 'STRING',
+					description: 'The name of the tag',
+					required: true,
+				}],
 			},
 			{
 				name: 'guide',
